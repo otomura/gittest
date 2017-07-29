3dbe181feedabe4ddbbc8014095d60134618beed
otomura <otomura@users.noreply.github.com>
2017-07-29 13:29:36 +0900

    js コミットします

---
+++ sample.js
@@ -0,0 +1,16 @@
+/*
+ * calc.js
+ * Copyright (C) 2014 kaoru <kaoru@bsd>
+ */
+ 
+/**
+ * Return the sum of a and b.
+ * @param {Number} a 1st number.
+ * @param {Number} b 2nd number.
+ * @returns {Number} Sum of number.
+ * @example
+ * こんな感じで書いてね！
+ */
+function sum(a,b) {
+        return a+b;
+}
